e0460295cef6c8dd74226e5c669efeff7a451ddb
todian-tornero <todanexus@gmail.com>
2021-03-21 17:49:39 +0300

    Сделал HealthActivity

--- app/src/main/java/com/example/lesson31/Health.java
+++ app/src/main/java/com/example/lesson31/Health.java
@@ -1,5 +1,19 @@
 package com.example.lesson31;
 
 public class Health {
+    private double weight;
+    private int steps;
 
+    public Health(double weight, int steps) {
+        this.weight = weight;
+        this.steps = steps;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
 }
--- app/src/main/java/com/example/lesson31/HealthActivity.java
+++ app/src/main/java/com/example/lesson31/HealthActivity.java
@@ -3,6 +3,10 @@ package com.example.lesson31;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class HealthActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class HealthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
+
+        clickOnButtonSaveHealth();
+    }
+
+    private void clickOnButtonSaveHealth(){
+        Button buttonSaveHealth = findViewById(R.id.buttonSaveHealth);
+        buttonSaveHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText weight = findViewById(R.id.editTextWeight);
+                EditText steps = findViewById(R.id.editTextSteps);
+                try {
+                    Health health = new Health(Double.parseDouble(weight.getText().toString()), Integer.parseInt(steps.getText().toString()));
+                }
+                catch (Exception e){
+                    Toast.makeText(HealthActivity.this, "Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
